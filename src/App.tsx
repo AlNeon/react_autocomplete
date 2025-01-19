624562daa0f350954d0c41fbedb918117a10609a
Oleksii Karpov <alneon1@gmail.com>
2025-01-19 11:28:21 +0200

    added solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import debounce from 'lodash.debounce';
+import classNames from 'classnames';
+import 'bulma';
+
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+import { DropdownMenu } from './components/DropdownMenu';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [title, setTitle] = useState('No selected person');
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, 300), []);
+
+  useEffect(() => {
+    if (selectedPerson && query === selectedPerson.name) {
+      setTitle(
+        `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`,
+      );
+    } else {
+      setTitle('No selected person');
+    }
+  }, [selectedPerson, query]);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const handleClearInput = () => {
+    setQuery('');
+    setSelectedPerson(null);
+    applyQuery('');
+  };
+
+  const filteredNames = useMemo(() => {
+    return peopleFromServer.filter(person =>
+      person.name.toLowerCase().includes(appliedQuery.trim().toLowerCase()),
+    );
+  }, [appliedQuery]);
+
+  const onSelected = (person: Person) => {
+    setSelectedPerson(person);
+    setQuery(person.name);
+    setIsFocused(false);
+  };
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {title}
         </h1>
 
-        <div className="dropdown is-active">
-          <div className="dropdown-trigger">
-            <input
-              type="text"
-              placeholder="Enter a part of the name"
-              className="input"
-              data-cy="search-input"
-            />
-          </div>
-
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
+        <div
+          className={classNames('dropdown', {
+            'is-active': isFocused && filteredNames.length > 0,
+          })}
+        >
+          <div className="dropdown-trigger field has-addons">
+            <div className="control is-expanded">
+              <input
+                type="text"
+                placeholder="Enter a part of the name"
+                className="input"
+                data-cy="search-input"
+                value={query}
+                onChange={handleQueryChange}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+              />
             </div>
+            {query && (
+              <div className="control">
+                <button
+                  className="button is-light"
+                  onClick={handleClearInput}
+                  data-cy="clear-button"
+                >
+                  Clear
+                </button>
+              </div>
+            )}
           </div>
+
+          <DropdownMenu filteredNames={filteredNames} onSelected={onSelected} />
         </div>
 
-        <div
-          className="
+        {filteredNames.length === 0 && (
+          <div
+            className="
             notification
             is-danger
             is-light
             mt-3
             is-align-self-flex-start
           "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </main>
     </div>
   );
